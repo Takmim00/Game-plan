a25cb4f94b6e5dff3d3b114a0f5be0de14433f18
Takmim00 <takmimm@gmail.com>
2025-08-02 20:37:42 +0600

    maximize section added

---
+++ src/component/Maximize.jsx
@@ -0,0 +1,61 @@
+import img from '../assets/game3.png'
+import play from "../assets/play-store-BbIiZpff.png"
+import app from "../assets/app-store-QtgANU77.png"
+const Maximize = () => {
+  return (
+    <div class="relative py-10 md:py-16 w-11/12 mx-auto">
+      <div class="relative flex flex-col lg:flex-row items-center justify-between">
+        <div class="w-full lg:w-1/2 text-center lg:text-left mb-8 lg:mb-0">
+          <h2 class="text-3xl md:text-4xl lg:text-5xl font-extrabold text-gray-900 mb-6 leading-tight">
+            Maximize <br /> Your Impact as <br /> a Coach.
+          </h2>
+          <p class="text-gray-600 text-sm md:text-base mb-8">
+            Our platform is designed to help sports coaches save time, stay
+            organized, and focus on what they love most—coaching. The ability to
+            create training sessions tailored specifically to your athletes
+            exact level means you can feel confident you’re delivering maximum
+            impact as a coach.
+          </p>
+          <div class="flex items-center justify-center lg:justify-start space-x-4">
+            <a
+              href=""
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={app}
+                alt="App Store"
+                class=""
+              />
+            </a>
+            <a
+              href=""
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={play}
+                alt="Google Play"
+                class=""
+              />
+            </a>
+          </div>
+        </div>
+        <div class="relative flex items-center justify-center">
+          <div class="absolute -top-16 lg:-top-6 right-24 w-56  hidden lg:block  h-56 lg:w-96 lg:h-96 rounded-full "></div>
+          <div class="relative z-20">
+            <div class=" overflow-hidden ">
+              <img
+                src={img}
+                alt="Dashboard"
+                class="w-[300px] md:w-[450px] lg:w-[550px] h-auto object-cover z-20"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Maximize;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../component/Banner";
 import Coaches from "../component/Coaches";
+import Maximize from "../component/Maximize";
 import Ultimate from "../component/Ultimate";
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
             <Banner/>
             <Ultimate/>
             <Coaches/>
+            <Maximize/>
         </div>
     );
 };
